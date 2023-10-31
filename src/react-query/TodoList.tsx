c302e33dd918f081b7d07468781a1a9947711007
andreajiao <andreajiao@gmail.com>
2023-10-31 08:35:46 +0100

    fetching data + errorhandling

---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const TodoList = () => {
+
+    const fetchTodos = ()=> axios
+      .get <Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .then(res=> res.data)
+
+    const { data: todos, error } = useQuery<Todo[], Error>({
+        queryKey: ['todos'],
+        queryFn: fetchTodos
+    })
+
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <ul className="list-group">
+      {todos?.map((todo) => (
+        <li key={todo.id} className="list-group-item">
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
