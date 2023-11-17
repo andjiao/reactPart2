a8a29eec5c7df03f1ebef985b9bc64b164844097
andreajiao <andreajiao@gmail.com>
2023-11-17 09:57:34 +0100

    it works with useInfiniteQuery, but error on line 22 in usePost-file with the pageParam

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 
 import './App.css'
 import PostList from './react-query/PostList ';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 
 function App() {
   return (
   
-    <QueryClientProvider client={queryClient} >
        <PostList/>
-    </QueryClientProvider>
-   
-  
   )
 }
 
--- src/react-query/hooks/usePosts.tsx
+++ src/react-query/hooks/usePosts.tsx
@@ -1,5 +1,5 @@
-import { useInfiniteQuery } from 'react-query';
-import axios from 'axios';
+ import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 interface Post {
   id: number;
@@ -13,9 +13,9 @@ interface PostQuery {
 }
 
 const usePosts = (query: PostQuery) =>
-  useInfiniteQuery <Post [], Error>(
-    ['posts', { ...query }],
-    ({ pageParam = 1 }) =>
+  useInfiniteQuery<Post[], Error>({
+    queryKey: ['posts', query],
+    queryFn: ({ pageParam = 1 }) =>
       axios
         .get('https://jsonplaceholder.typicode.com/posts', {
           params: {
@@ -24,14 +24,12 @@ const usePosts = (query: PostQuery) =>
           },
         })
         .then((res) => res.data),
-    {
-      getNextPageParam: (lastPage, allPages) => {
-        // Assuming the API response has a property 'length' indicating the number of items in the next page
-        return lastPage.length > 0 ? allPages.length+1 : undefined;
-      },
-      staleTime: 60 * 1000, // 1min
-      keepPreviousData: true,
-    }
-  );
+    getNextPageParam: (lastPage, allPages) => {
+      // Assuming the API response has a property 'length' indicating the number of items in the next page
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
+    staleTime: 1 * 60 * 1000, // 1min
+    initialPageParam: 1
+  });
 
 export default usePosts;
